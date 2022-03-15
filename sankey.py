166e3936c5bcb0900c434aaaed1e71ff9b60a11c
sgno28 <s.ngotuantu@gmail.com>
2022-03-15 23:41:13 +0000

    commit

--- sankey.py
+++ sankey.py
@@ -6,7 +6,7 @@ from ezgraphics import GraphicsWindow
 
 WIDTH = 1000        # Width of the window in pixels
 HEIGHT = 700        # Height of the window in pixels
-GAP = 10            # Gap between disagram arrows in pixels
+GAP = 100            # Gap between disagram arrows in pixels
 COLOURS = [(230, 25, 75), (60, 180, 75), (255, 225, 25), (0, 130, 200),
 (245,	130,	48),	(145,	30, 180), (70, 240,	240),	(240, 50, 230),
 (210,	245,	60),	(250,	190, 212), (0, 128,	128),	(220, 190, 255),
@@ -17,14 +17,11 @@ COLOURS = [(230, 25, 75), (60, 180, 75), (255, 225, 25), (0, 130, 200),
 def read_file(file_name):
     """Opens and reads the file. Returns the title, left-hand axis label and 
     the data values in the file.
-
     Args:
         file_name (str): file containing the data.
-
     Raises:
         FileNotFoundError: If file not found or is not readable, 
                             this exception is raised
-
     Returns:
         str: diagram title
         str: left-hand axis label
@@ -48,7 +45,6 @@ def read_file(file_name):
 def set_up_graph(title):
     """Creates a window and canvas. Displays the title, left-hand axis label.
     Returns a reference to the window. 
-
     Args:
         title (str): title for the window
         
@@ -75,13 +71,10 @@ def parse_value (str, line_number) :
 
 def process_data(data_list) :
     """Returns a dictionary produced by processing the data in the list. 
-
     Args:
         data_list (list): list containing the data read from the file
-
     Raises:
         ValueError: raised if there are errors in the data values in the file
-
     Returns:
         dictionary: contains data about the flows
     """
@@ -107,9 +100,30 @@ def process_data(data_list) :
     else:
         return ValueError         
 
-def draw_sankey(window, title, data_dic, gap_size = 100, border_size = 100):
-    """Draw the sankey diagram
+def get_colour(c):
+    return COLOURS[c][0], COLOURS[c][1], COLOURS[c][2]
 
+def draw_source(canvas, title, s_start, s_width, s_height, border_size = 100):
+    canvas.setFill(0,0,0)
+    canvas.drawRectangle(s_start, border_size, s_width, s_height) #draw the source rectangle
+    canvas.setTextAnchor("center")
+    canvas.setColor(255, 255, 255)
+    canvas.drawText((WIDTH / 2), 125, title)
+    
+def colour_grad(canvas, s_start, n, s_low, pixel, r , g, b):
+    x = s_start #x corrdinate initially for the lines
+    h = (HEIGHT - 150) - s_low #height between the source low and destination high
+    shift = n - s_start  #difference between the x cordinate of the destination and source block
+    dx = shift / h #shift per unit height
+    dr, dg, db = (r/h), (g/h), (b/h) #change in colour per line 
+    for i in range(0, h+1): #from the start to the maximum height
+        r , g, b = round(i*dr), round(i*dg), round(i*db)  
+        canvas.setColor(r, g, b)
+        canvas.drawLine(x + 1, i + s_low, x + pixel, i + s_low)
+        x = x + dx
+
+def draw_dest(canvas, key, pixel, n, start, low, c):
+    """Draw the shapes in the diagram such as the destination and connectors
     Args:
         window (GraphicsWindow): contains the graph
         title (string): contains the label to overlay on the source arrow
@@ -117,7 +131,44 @@ def draw_sankey(window, title, data_dic, gap_size = 100, border_size = 100):
         gap_size (int): number of pixels to leave between destination arrows
         border_size (int): Minimum separation to othe edges of the window
     """
-    pass
+    r, g, b = get_colour(c)
+    canvas.setColor(0, 0, 0)
+    canvas.setFill(r, g, b) #sets colour of destinations
+    canvas.drawPolygon(n, (HEIGHT - 150), (n+pixel), (HEIGHT - 150),\
+        (n + (n + pixel))/2, HEIGHT - 100) #contructs the destinations
+    canvas.setColor(0,0,0)
+    canvas.drawPolygon(start, low, start +\
+            pixel, low, n+pixel, HEIGHT - 150, n, (HEIGHT - 150)) #draws the connectors
+    colour_grad(canvas, start, n, low, pixel,r, g, b) #construct the color gradient
+    canvas.setColor(255-r, 255 - g, 255 - b) #sets colour of the destination label
+    canvas.drawText((n + (n + pixel))/2, HEIGHT - 150, key) #forms the destination label
+
+def draw_sankey(window, title, data_dic, gap_size = 100, border_size = 100):
+    """Draw the sankey diagram
+    Args:
+        window (GraphicsWindow): contains the graph
+        title (string): contains the label to overlay on the source arrow
+        data_dic (dictionary): contains the data for the graph
+        gap_size (int): number of pixels to leave between destination arrows
+        border_size (int): Minimum separation to othe edges of the window
+    """
+    n = border_size
+    data_flow = sum(data_dic.values()) #values sumd
+    diagram_width = 1000 - (2*100) - ((len(data_dic) - 1)*gap_size) #total available width to use
+    ppf = diagram_width / data_flow #pixels per flow
+    s_width = ppf * data_flow
+    s_start = (WIDTH/2)-(s_width/2)
+    s_height = 50
+    s_low = border_size + s_height
+    canvas = window.canvas() #start drawing
+    draw_source(canvas, title, s_start, s_width, s_height, border_size) #contructs the source block
+    iteration = 0 #keep track of how many destinations have been plotted to get their color
+    for key in data_dic:
+        p_data = data_dic[key] * ppf # calculates the pixel width for each destination
+        draw_dest(canvas, key, p_data, n, s_start, s_low, iteration) #contructs the desitinations and connecters
+        n = n + p_data + gap_size #moves the starting point for the destinations
+        s_start = s_start + p_data #moves starting point for the source
+        iteration += 1 #increases after each destination has been plotted
 
 def main():
     # DO NOT EDIT THIS CODE
--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 
 from ezgraphics import GraphicsWindow
 
+COLOURS = [(230, 25, 75), (60, 180, 75), (255, 225, 25), (0, 130, 200),
+(245,	130,	48),	(145,	30, 180), (70, 240,	240),	(240, 50, 230),
+(210,	245,	60),	(250,	190, 212), (0, 128,	128),	(220, 190, 255),
+(170, 110, 40), (255, 250, 200), (128, 0, 0), (170, 255, 195),
+(128,	128,	0), (255, 215, 180), (0, 0, 128), (128, 128, 128)]
+WIDTH = 1000        # Width of the window in pixels
+HEIGHT = 700        # Height of the window in pixels
+GAP = 10
+
 def readBigString(file_name):
     try :
         dataFile = open(file_name, "r")
@@ -58,45 +67,67 @@ def clean_list(dlist):
     else:
         return ValueError
 
-def eg(window, title, data_dic, gap_size = 100, border_size = 100):
-    WIDTH = 1000
-    HEIGHT = 700
-    win = GraphicsWindow(WIDTH, HEIGHT)
-    win.setTitle(title)
-    border_size = 100
-    n = border_size
-    data_flow = sum(data_dic.values()) #values sumd
-    diagram_width = 1000 - (2*100) - ((len(data_dic) - 1)*gap_size) #total width to use
-    ppf = diagram_width / data_flow #pixels per flow
-    source_width = ppf * data_flow
-    source_start = (WIDTH/2)-(source_width/2)
-    source_height = 50
-    source_low = border_size + source_height
-    #start drawing
-    canvas = win.canvas()
-    canvas.drawRectangle(100,100, (800), (500)) #margins
-    canvas.setFill()
-    canvas.drawRectangle(source_start, border_size, source_width, source_height) #draw the source rectangle
+def get_colour(c):
+    return COLOURS[c][0], COLOURS[c][1], COLOURS[c][2]
+
+def draw_source(canvas, title, s_start, s_width, s_height, border_size = 100):
+    canvas.setFill(0,0,0)
+    canvas.drawRectangle(s_start, border_size, s_width, s_height) #draw the source rectangle
     canvas.setTextAnchor("center")
+    canvas.setColor(255, 255, 255)
     canvas.drawText((WIDTH / 2), 125, title)
     
+def colour_grad(canvas, s_start, n, s_low, pixel, r , g, b):
+    x = s_start #x corrdinate initially for the lines
+    h = (HEIGHT - 150) - s_low #height between the source low and destination high
+    shift = n - s_start  #difference between the x cordinate of the destination and source block
+    dx = shift / h #shift per unit height
+    dr, dg, db = (r/h), (g/h), (b/h) #change in colour per line 
+    for i in range(0, h+1): #from the start to the maximum height
+        r , g, b = round(i*dr), round(i*dg), round(i*db)  
+        canvas.setColor(r, g, b)
+        canvas.drawLine(x + 1, i + s_low, x + pixel, i + s_low)
+        x = x + dx
+
+def draw_dest(canvas, key, pixel, n, start, low, c):
+    r, g, b = get_colour(c)
+    canvas.setColor(0, 0, 0)
+    canvas.setFill(r, g, b) #sets colour of destinations
+    canvas.drawPolygon(n, (HEIGHT - 150), (n+pixel), (HEIGHT - 150),\
+        (n + (n + pixel))/2, HEIGHT - 100) #contructs the destinations
+    canvas.setColor(0,0,0)
+    canvas.drawPolygon(start, low, start +\
+            pixel, low, n+pixel, HEIGHT - 150, n, (HEIGHT - 150)) #draws the connectors
+    colour_grad(canvas, start, n, low, pixel,r, g, b) #construct the color gradient
+    canvas.setColor(255-r, 255 - g, 255 - b) #sets colour of the destination label
+    canvas.drawText((n + (n + pixel))/2, HEIGHT - 150, key) #forms the destination label
+    
+def eg(win, title, data_dic, gap_size = 100, border_size = 100):
     n = border_size
+    data_flow = sum(data_dic.values()) #values sumd
+    diagram_width = 1000 - (2*100) - ((len(data_dic) - 1)*gap_size) #total available width to use
+    ppf = diagram_width / data_flow #pixels per flow
+    s_width = ppf * data_flow
+    s_start = (WIDTH/2)-(s_width/2)
+    s_height = 50
+    s_low = border_size + s_height
+    canvas = win.canvas() #start drawing
+    draw_source(canvas, title, s_start, s_width, s_height, border_size) #contructs the source block
+    iteration = 0 #keep track of how many destinations have been plotted to get their color
     for key in data_dic:
-        p_data = data_dic[key] * ppf
-        canvas.setFill(0,0,0)
-        canvas.drawPolygon(n, (HEIGHT - 150), (n+p_data), (HEIGHT - 150), (n + (n + p_data))/2, HEIGHT - 100)
-        canvas.drawText((n + (n + p_data))/2, HEIGHT - 150, key)
-        canvas.drawPolygon(source_start, source_low,  n, (HEIGHT - 150), source_start + p_data, source_low, n+p_data, HEIGHT - 150)
-        n = n + p_data + gap_size
-        source_start = source_start + p_data
-    #wait to close
-    win.wait()
+        p_data = data_dic[key] * ppf # calculates the pixel width for each destination
+        draw_dest(canvas, key, p_data, n, s_start, s_low, iteration) #contructs the desitinations and connecters
+        n = n + p_data + gap_size #moves the starting point for the destinations
+        s_start = s_start + p_data #moves starting point for the source
+        iteration += 1 #increases after each destination has been plotted
 
 def main():
     title, label, data_list = readingLBL("netball_2018.txt")
     dict_data = clean_list(data_list)
-    eg(title, label, dict_data)
-    
+    win = GraphicsWindow(WIDTH, HEIGHT)
+    win.setTitle(title)
+    eg(win, label, dict_data)
+    win.wait()
     
 
 if __name__ == "__main__":
